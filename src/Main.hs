7be308f12c82ca64de3a7faebe7712d29bdbc670
Yann Herklotz <ymherklotz@gmail.com>
2018-09-28 12:19:53 +0100

    Remove unnecessary testing in main
    This will be moved to the testing module where the parsing and
    simplifying will be tested with quickcheck.

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,7 @@
 module Main where
 
 import LambdaCalc.Parser
-  ( mparse
-  )
-
 import LambdaCalc.Types
-import LambdaCalc.PrettyPrinter
 
 main :: IO ()
-main = putStrLn $ pprint (FuncAppl (FuncDef "x" (FuncDef "y" (FuncAppl (Var 'y') (Var 'x')))) (FuncDef "x" (Var 'x')))
+main = mparse
