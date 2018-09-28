f3db3f6d6275e039031021b79fafd682872687b7
Yann Herklotz <ymherklotz@gmail.com>
2018-09-28 12:21:05 +0100

    Add QuickCheck Dependency and example test
    QuickCheck dependency added to stack for further testing in the
    future.

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,3 +1,20 @@
-module Test.LambdaCalc where
+{-# LANGUAGE TemplateHaskell #-}
 
+module Main where
+
+import System.Exit (exitFailure)
 import Test.QuickCheck
+
+prop_commutativeAdd :: Int -> Int -> Bool
+prop_commutativeAdd x y = x + y == y + x
+
+return []
+runTests :: IO Bool
+runTests = $quickCheckAll
+
+main :: IO ()
+main = do
+  result <- runTests
+  case result of
+    True -> return ()
+    _ -> exitFailure
